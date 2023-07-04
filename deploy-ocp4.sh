9281de1471a982759a40ec4550029a6e2bda1eb3
Jorge Tudela <jtudelag@redhat.com>
2023-07-04 17:11:31 +0200

    Deploy single nic

--- deploy-ocp4.sh
+++ deploy-ocp4.sh
@@ -2,4 +2,6 @@
 
 sudo cp agent/agent.x86_64.iso /var/lib/libvirt/images/
 
-kcli create plan -f kcli-plans/sno-connected-plan.yaml
+#kcli create plan -f kcli-plans/sno-connected-plan.yaml
+
+kcli create plan -f kcli-plans/sno-connected-one-nic-plan.yaml
