6f8930a708a4fe6431e1bee755a2ed1f3d139a63
Jorge Tudela <jtudelag@redhat.com>
2023-07-04 17:10:32 +0200

    Record *-config.yaml as logs. Use single NIC agent

--- create-iso.sh
+++ create-iso.sh
@@ -6,13 +6,18 @@ rm -rf ./agent/*
 rm -rf ./agent/.openshift_install.log
 rm -rf ./agent/.openshift_install_state.json
 
-
 ssh-keygen -b 2048 -t rsa -f ./agent/is_rsa -q -N ""
 
 export PULL_SECRET=$(cat ./secrets/pull-secret.txt)
 export SSH_KEY=$(cat ./agent/is_rsa.pub)
 
 envsubst < ./templates/install-config.yaml.template > ./agent/install-config.yaml
-cp ./templates/agent-config.yaml ./agent/
+cp ./templates/agent-config-one-nic.yaml ./agent/agent-config.yaml
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
+mkdir "logs/$TIMESTAMP"
+
+cp ./agent/install-config.yaml "logs/$TIMESTAMP/"
+cp ./agent/agent-config.yaml "logs/$TIMESTAMP/"
 
 openshift-install --dir ./agent/ agent create image
