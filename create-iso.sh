5a68c694dba5f992953fd80d870cc4466965d141
Jorge Tudela <jtudelag@redhat.com>
2023-07-04 13:21:22 +0200

    More fixes

--- create-iso.sh
+++ create-iso.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 rm -rf ./agent/*
+
+# Hidden files, but .gitkeep
+rm -rf ./agent/.openshift_install.log
+rm -rf ./agent/.openshift_install_state.json
+
+
 ssh-keygen -b 2048 -t rsa -f ./agent/is_rsa -q -N ""
 
-PULL_SECRET=$(cat ./secrets/pull-secret.txt)
-SSH_KEY=$(cat ./agent/is_rsa.pub)
+export PULL_SECRET=$(cat ./secrets/pull-secret.txt)
+export SSH_KEY=$(cat ./agent/is_rsa.pub)
 
 envsubst < ./templates/install-config.yaml.template > ./agent/install-config.yaml
 cp ./templates/agent-config.yaml ./agent/
