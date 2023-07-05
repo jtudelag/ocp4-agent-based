bd7371e69ff2d271de1c65b660f9846fe2d558a0
Jorge Tudela <jtudelag@redhat.com>
2023-07-05 12:05:19 +0200

    bond interfaces and verbose bash

--- deploy-ocp4.sh
+++ deploy-ocp4.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -x
+
 sudo cp agent/agent.x86_64.iso /var/lib/libvirt/images/
 
-#kcli create plan -f kcli-plans/sno-connected-plan.yaml
+kcli create plan -f kcli-plans/sno-connected-plan.yaml
 
-kcli create plan -f kcli-plans/sno-connected-one-nic-plan.yaml
+#kcli create plan -f kcli-plans/sno-connected-one-nic-plan.yaml
